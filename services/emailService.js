7392669fbfc273ee65f6fe9b48f12655313878d0
Aditya Yadav <ay5480620@gmail.com>
2022-09-21 23:54:59 +0530

    initial commit

---
+++ services/emailService.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+async function sendMail({ from, to, subject, text, html }) {
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `inShare <${from}>`,
+    to: to, // list of receivers
+    subject: subject,
+    text: text,
+    html: html,
+  });
+
+}
+
+module.exports = sendMail;
