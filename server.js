985aa4bb1981a4e4f500bf8ea9c35e7ebd8051ed
Aditya Yadav <ay5480620@gmail.com>
2022-09-23 15:20:45 +0530

    initial commit

--- server.js
+++ server.js
@@ -27,13 +27,9 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // Routes
-app.use("api/files", require("./routes/files"));
-app.use("files", require("./routes/show"));
-app.use("files/download", require("./routes/download"));
-
-app.get('/',(req,res)=>{
-  res.render('index')
-})
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
 app.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${PORT}`);
